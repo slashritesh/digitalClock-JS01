372b3ef8a38b5edbdff925cda7460dcfccb11d68
RITESH MANE <maneritesh@gmail.com>
2023-08-24 22:06:19 +0530

    trying to add Zero

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const sec = document.querySelector('.sec')
 const min = document.querySelector('.min')
 
 
+
 const Ticking = function () {
     let time = new Date()
     
     hrs.textContent = time.getHours() % 12 || 12
     sec.textContent = time.getSeconds()
     min.textContent = time.getMinutes()
+
+
 }
 
 setInterval(Ticking,1000)
